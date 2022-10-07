5bba4dc7348da63b033bb425114bd00eb5204e50
Xingming <zengxm@beagledata.com>
2022-10-07 11:32:32 +0800

    add a transfer model based on convlstm

--- PCIC2022/code/2-convlstm/main.py
+++ PCIC2022/code/2-convlstm/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from model import ConvLSTM
 from processer import Processer
 
-def main():
+def main_strategy_1():
     raw_train_data = np.load('../../data/example/cityA/X.npy')
     raw_train_label = np.load('../../data/example/cityA/Y.npy')
     raw_test_data = np.load('../../data/example/cityB/X.npy')
@@ -19,11 +19,51 @@ def main():
                 model_test_data, model_test_label)
 
     raw_test_data = np.load('../../data/example/cityB/X.npy')
-
     model_test_data = processer.transform(raw_test_data)
     pred_res = model.predict(model_test_data)
+    pd.DataFrame(pred_res).to_csv('result/v1/submission.csv')
 
-    pd.DataFrame(pred_res).to_csv('./submission.csv')
+def main_strategy_2():
+    from sklearn.model_selection import train_test_split
+
+    # model_baseline
+    raw_data = np.load('../../data/train/cityA/X.npy')
+    raw_label = np.load('../../data/train/cityA/Y.npy')
+    raw_train_data,raw_test_data, raw_train_label, raw_test_label = train_test_split(
+      raw_data,
+      raw_label,
+      test_size=0.3,
+      random_state=0
+    )
+    processer = Processer()
+    model_train_data, model_train_label = processer.fit(raw_train_data, raw_train_label)
+    model_test_data, model_test_label = processer.transform(raw_test_data, raw_test_label)
+
+    model = ConvLSTM(model_train_data.shape[1:])
+    model.train(model_train_data, model_train_label,
+                model_test_data, model_test_label)
+
+    # model_transfer
+    raw_data = np.load('../../data/train/cityB/train/X.npy')
+    raw_label = np.load('../../data/train/cityB/train/Y.npy')
+    raw_train_data,raw_test_data, raw_train_label, raw_test_label = train_test_split(
+      raw_data,
+      raw_label,
+      test_size=0.3,
+      random_state=0
+    )
+    processer = Processer()
+    model_train_data, model_train_label = processer.fit(raw_train_data, raw_train_label)
+    model_test_data, model_test_label = processer.transform(raw_test_data, raw_test_label)
+
+    model.transfer(model_train_data, model_train_label,
+                   model_test_data, model_test_label)
+
+    # use model_transfer to predict out_of_sample
+    raw_test_data = np.load('../../data/test/X.npy')
+    model_test_data = processer.transform(raw_test_data)
+    pred_res = model.predict(model_test_data)
+    pd.DataFrame(pred_res).to_csv('result/v2/submission.csv')
 
 if __name__ == '__main__':
-    main()
+    main_strategy_2()
--- PCIC2022/code/2-convlstm/model.py
+++ PCIC2022/code/2-convlstm/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.utils import class_weight
+from matplotlib import pyplot as plt
 
 import os
 os.environ['CUDA_VISIBLE_DEVICES']='0'
@@ -8,6 +9,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Activation
 from tensorflow.keras.layers import RepeatVector, TimeDistributed
 from tensorflow.keras.layers import ConvLSTM2D, Flatten
+from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
 
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -53,19 +55,49 @@ class ConvLSTM(object):
         seq.add(Activation('sigmoid'))
         self.model = seq
 
-    def train(self, X_train, Y_train, X_test, Y_test):
+    def train(self, X_train, Y_train, X_val, Y_val, cp_name='model_baseline'):
         cw = class_weight.compute_class_weight(class_weight='balanced',
                                                classes=np.unique(Y_train),
                                                y=Y_train)
         cw = dict(enumerate(cw))
 
-        self.model.compile(
-            loss='binary_crossentropy',
-            optimizer='adam')
+        plateau = ReduceLROnPlateau(monitor="val_loss",
+                                    verbose=0,
+                                    mode='min',
+                                    factor=0.1,
+                                    patience=6)
+        early_stopping = EarlyStopping(monitor='val_loss',
+                                       verbose=0,
+                                       mode='min',
+                                       patience=20)
+        checkpoint = ModelCheckpoint(f'./checkpoints/{cp_name}.h5',
+                                     monitor='val_loss',
+                                     verbose=1,
+                                     mode='min',
+                                     period=1,
+                                     save_weights_only=True,
+                                     save_best_only=True)
 
-        self.model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
-                      class_weight=cw, batch_size=16, epochs=100, verbose=1)
+        self.model.compile(loss='binary_crossentropy',
+                           optimizer='adam')
+
+        history = self.model.fit(X_train, Y_train, validation_data=(X_val, Y_val),
+                                 class_weight=cw, batch_size=16, epochs=30, verbose=1, callbacks=[plateau, early_stopping, checkpoint])
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('ConvLSTM: Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.show()
         return self
+
+    def transfer(self, X_train, Y_train, X_val, Y_val):
+        self.load_weights(f'./checkpoints/model_baseline.h5')
+        self.model.trainable = True
+        for layer in self.model.layers[:-2]:
+            layer.trainable = False
+        self.train(X_train, Y_train, X_val, Y_val, 'model_transfer')
 
     def predict(self, X_test):
         Y_pre = self.model.predict(X_test).reshape(-1)
