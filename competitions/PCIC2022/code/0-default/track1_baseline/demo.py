00dd1e35f12b5bc1f887cb9ce59d15438ba215f6
Xingming <zengxm@beagledata.com>
2022-10-18 11:32:23 +0800

    adjusted dataset path in demo.py

--- competitions/PCIC2022/code/0-default/track1_baseline/demo.py
+++ competitions/PCIC2022/code/0-default/track1_baseline/demo.py
@@ -6,9 +6,9 @@ from sasa.estimator import SASA_Classiffer
 
 if __name__ == '__main__':
     # data path
-    src_train_path = os.path.join("Dataset/phase1_TrainData/cityA/X.npy")
-    tgt_train_path = os.path.join("Dataset/phase1_TrainData/cityB/train/X.npy")
-    tgt_test_path = os.path.join("Dataset/phase1_TestData/X.npy")
+    src_train_path = os.path.join("../../../data/train/cityA/X.npy")
+    tgt_train_path = os.path.join("../../../data/train/cityB/train/X.npy")
+    tgt_test_path = os.path.join("../../../data/test/X.npy")
 
     clsf = SASA_Classiffer(input_dim=10, training_steps=100)
     clsf.fit(src_train_path, tgt_train_path)
